282ce882f509cbafbb16ff255c69dba1568ed05c
kaz_mat <kazmat.hui@gmail.com>
2022-07-05 21:40:44 +0900

    258_a_b

--- abc/258/a.py
+++ abc/258/a.py
@@ -1,6 +1,10 @@
 
-s = input()
-sum = 0
-for i in range(len(s)):
-    sum += int(s[i])
-print(45-sum)
+k = int(input())
+if 0 <= k < 10:
+    print("21:0" + str(k))
+if 10 <= k < 60:
+    print("21:" + str(k))
+if 60 <= k < 70:
+    print("22:0" + str(k-60))
+if k >= 70:
+    print("22:" + str(k-60))
--- abc/258/b.py
+++ abc/258/b.py
@@ -1,9 +1,18 @@
-a, b, k = map(int, input().split())
+N = int(input())
+A = [list(map(int, list(input()))) for _ in range(N)]
+ 
+max_li = []
+max_v = -1
+ 
 ans = 0
-while True:
-  if a >= b:
-    break
-  a = a * k
-  ans += 1
-
+for i in range(N):
+    for j in range(N):
+        for k in range(-1, 2):
+            for l in range(-1, 2):
+                if k == l == 0:
+                    continue
+                r = 0
+                for m in range(N):
+                    r = r * 10 + A[(i + k * m) % N][(j + l * m) % N]
+                ans = max(ans, r)
 print(ans)
