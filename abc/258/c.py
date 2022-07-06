b5d2e6646b609df45d1af24c57d143d32e5842f3
kaz_mat <kazmat.hui@gmail.com>
2022-07-06 19:36:54 +0900

    258_c

--- abc/258/c.py
+++ abc/258/c.py
@@ -1,6 +1,10 @@
-n, m, k = map(int, input().split())
-
-
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        
+n, q = map(int,input().split())
+S = input()
+ 
+x = 0
+for _ in range(q):
+    i, j = map(int, input().split())
+    if i == 1:
+        x += j
+    else:
+        print(S[(j-x-1)%n])
