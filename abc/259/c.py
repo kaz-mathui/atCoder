dc6496b8df5c5a7b69f10b751ee5fcb86e5963d9
kaz_mat <kazmat.hui@gmail.com>
2022-07-12 21:59:44 +0900

    259 c

--- abc/259/c.py
+++ abc/259/c.py
@@ -1,20 +1,44 @@
-from collections import defaultdict
-s = input()
-t = input()
 
-dict = defaultdict(int)
-dict_t = defaultdict(int)
-
-for i in range(len(s)):
-    dict[s[i]] += 1
-
-for i in range(len(t)):
-    dict_t[t[i]] += 1
-
-
-for key,value in dict_t.items():
-    if value < dict[key] or value >= 2 and dict[key] < 2:
-        print('No')
-        exit()
-
-print('Yes')
+S = list(input())
+T = list(input())
+ 
+S_count = []
+count = -1
+str = ''
+for i in S:
+    if str != i:
+        str = i
+        S_count.append([str, 1])
+        count += 1
+    else:
+        S_count[count][1] += 1
+ 
+T_count = []
+count = -1
+str = ''
+for i in T:
+    if str != i:
+        str = i
+        T_count.append([str, 1])
+        count += 1
+    else:
+        T_count[count][1] += 1
+ 
+ans = 'Yes'
+count = 0
+for j in S_count:
+    if j[0] != T_count[count][0]:
+        ans = 'No'
+        break
+    elif j[1] == 1 and T_count[count][1] != 1:
+        ans = 'No'
+        break
+    elif j[1] > T_count[count][1]:
+        ans = 'No'
+        break
+    count += 1
+ 
+if len(S_count) == len(T_count):
+    print(ans)
+else:
+    print('No')
