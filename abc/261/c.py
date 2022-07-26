95baf9d20cad752bd3a043bfceff5ae82664b5c1
kaz-mathui <kazmat.hui@gmail.com>
2022-07-27 06:57:10 +0900

    add c

--- abc/261/c.py
+++ abc/261/c.py
@@ -1,15 +1,2 @@
-n, x, y = map(int, input().split())
-
-def red(n):
-    if n == 1:
-        return 0
-    else:
-        return red(n-1) + x * blue(n)
- 
-def blue(n):
-    if n == 1:
-        return 1
-    else:
-        return y * blue(n-1) + red(n-1)
- 
-print(red(n))
+n, x, y = list(map(int, input().split()))
+print(n,x,y)
