8860dfa4f04ee53a0c5e2266b581ca3171b6ff2f
kaz-mathui <kazmat.hui@gmail.com>
2022-08-16 10:21:05 +0900

    add c

--- abc/250/c.py
+++ abc/250/c.py
@@ -1,13 +1,18 @@
-N = int(input())
-winner = None
-max_score = 0
-words = set()
-for i in range(N):
-    S, T = input().split()
-    T = int(T)
-    if not S in words:
-        words.add(S)
-        if max_score < T:
-            max_score = T
-            winner = i
-print(winner+1)
+n, q = map(int, input().split())
+data = [int(input()) for _ in range(q)] 
+A = list(range(n+1))
+index = list(range(n+1))
+
+for i in data:
+  p1 = index[i]
+  if p1 != n:
+    p2 = p1 + 1
+  else:
+    p2 = p1 - 1
+  y = A[p2]
+  A[p1],A[p2] = A[p2],A[p1]
+  index[i] = p2
+  index[y] = p1
+  
+
+print(*A[1:])
