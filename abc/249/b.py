df41742eb87405bd184d3b14672c81fce73e79b9
kaz-mathui <kazmat.hui@gmail.com>
2022-08-18 06:30:11 +0900

    add b

--- abc/249/b.py
+++ abc/249/b.py
@@ -1,17 +1,6 @@
-n, a, b = map(int, input().split())
-for i in range(n * a):
-    ans = ''
-    if (i // a) % 2 == 0:
-        
-        for j in range(n * b):
-            if (j // b) % 2 == 0:
-                ans += '.'
-            else:
-                ans += '#'
-    else:
-        for j in range(n * b):
-            if (j // b) % 2 == 0:
-                ans += '#'
-            else:
-                ans += '.'
-    print(ans)
+S = input()
+ 
+if not S.isupper() and not S.islower() and len(S) == len(set(S)):
+    print('Yes')
+else:
+    print('No')
