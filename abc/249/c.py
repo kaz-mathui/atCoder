7226466046d828ae920a3d5c8a2c846cc95980ee
kaz-mathui <kazmat.hui@gmail.com>
2022-08-19 07:05:06 +0900

    add c

--- abc/249/c.py
+++ abc/249/c.py
@@ -1,18 +1,17 @@
-n, q = map(int, input().split())
-data = [int(input()) for _ in range(q)] 
-A = list(range(n+1))
-index = list(range(n+1))
-
-for i in data:
-  p1 = index[i]
-  if p1 != n:
-    p2 = p1 + 1
-  else:
-    p2 = p1 - 1
-  y = A[p2]
-  A[p1],A[p2] = A[p2],A[p1]
-  index[i] = p2
-  index[y] = p1
-  
-
-print(*A[1:])
+from itertools import combinations
+ 
+ 
+N, K = map(int, input().split())
+ 
+S = [input() for _ in range(N)]
+ 
+ans = 0
+for r in range(N):
+    for comb in combinations(S, r+1):
+        cnt = [0]*26
+        for s in comb:
+            for c in s:
+                cnt[ord(c) - ord('a')] += 1
+        ans = max(ans, cnt.count(K))
+ 
+print(ans)
