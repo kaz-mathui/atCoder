88d78d3880f4e2717b6ab9e7cd944e92a5686e28
kaz-mathui <kazmat.hui@gmail.com>
2022-08-25 06:46:04 +0900

    add c

--- abc/245/c.py
+++ abc/245/c.py
@@ -1,13 +1,17 @@
-N,K,X = map(int,input().split())
-A = list(map(int,input().split()))
-for i in range(N):
-    m = min(K, A[i]//X)
-    A[i] -= X*m
-    K -= m
-A.sort(reverse=True)
-for i in range(N):
-    if 0 < K:
-        A[i] = 0
-        K -= 1
- 
-print(sum(A))
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+s = [a[0], b[0]]
+for i in range(1, n):
+    t = [0, 0]
+    for item in s:
+        if item == 0: continue
+        if abs(item - a[i]) <= k:
+            t[0] = a[i]
+        if abs(item - b[i]) <= k:
+            t[1] = b[i]
+    if sum(t) == 0:
+        print('No')
+        exit()
+    s[0], s[1] = t[0], t[1]
+print('Yes')
