55057f3e9cc099333f375ae2a2023703f4ec800c
kaz-mathui <kazmat.hui@gmail.com>
2022-08-27 18:12:05 +0900

    add b

--- abc/244/b.py
+++ abc/244/b.py
@@ -1,18 +1,11 @@
-n = input()
-a = list(map(int, input().split()))
- 
-a.sort()
-ans = 0
- 
-if a[0] != 0:
-    print(0)
-    exit()
- 
-for i in a:
-    if i - ans == 1:
-        ans += 1
-    elif i - ans > 1 :
-        print(ans+1)
-        exit()
- 
-print(ans+1)
+n=int(input())
+t=list(input())
+s=[0,0]
+d=[1,-1,-1,1]
+i=0
+for tt in t:
+  if tt=='S':
+    s[i%2]+=d[i%4]
+  else:
+    i+=1
+print(*s)
